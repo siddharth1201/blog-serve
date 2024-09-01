982e24f4b60d524b42109bca92a628f10f8c76d8
siddharth1201 <siddharthverma1201@gmail.com>
2024-09-01 16:05:54 +0530

    seed data added

--- src/main/java/com/example/blog_server/security/AppSecurityConfig.java
+++ src/main/java/com/example/blog_server/security/AppSecurityConfig.java
@@ -51,10 +51,10 @@ public class AppSecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Adjust this to your frontend URL
+                        .allowedOrigins("*") // Adjust this to your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
